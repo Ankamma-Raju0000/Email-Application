0b50d6c24898aa590e3b25c34d81ac9ec406a3f8
Ankamma-Raju0000 <ankammaraju0000@gmail.com>
2025-11-09 21:56:18 +0000

    Create EmailApp.java

---
+++ EmailApp.java
@@ -0,0 +1,12 @@
+package com;
+
+public class EmailApp 
+{
+	public static void main(String[] args) 
+	{
+		Email email=new Email("raju", "Tammisetty");
+		System.out.println(email.showInfo());
+		
+	}
+
+}
